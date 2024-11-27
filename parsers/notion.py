b07f83fe3f1af7873b93fa680872578862a57906
Kin1599 <dkonopliannikov05@mail.ru>
2024-11-28 00:52:16 +0300

    auto install driver

--- parsers/notion.py
+++ parsers/notion.py
@@ -1,51 +1,61 @@
 
 import time
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 import fake_useragent
+import os
 
-user_agent = fake_useragent.UserAgent()
+def fetch_and_save_notion_content(url, file_path='./parsers/results/notion_content.txt'):
+    user_agent = fake_useragent.UserAgent()
 
-chrome_driver_path = "C:\DriversSelenium\chromedriver\chromedriver-win64-version-131.0.6778.86\chromedriver.exe"
+    options = webdriver.ChromeOptions()
+    options.add_argument(f"user-agent={user_agent.random}")
+    options.add_argument("--headless")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-options = webdriver.ChromeOptions()
-options.add_argument(f"user-agent={user_agent.random}")
-options.add_argument("--headless")
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_service = Service(ChromeDriverManager().install())
+    browser = webdriver.Chrome(service=chrome_service, options=options)
 
-chrome_service = Service(chrome_driver_path)
-browser = webdriver.Chrome(service=chrome_service, options=options)
+    browser.get(url)
 
-website = 'https://quickest-custard-3d3.notion.site/Dmitry-Konoplyannikov-7892b63ba7cb4000b45484147a783bf0'
+    try:
+        time.sleep(5)
+        WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+        print("Страница успешно загружена")
 
-browser.get(website)
+        notion_app_element = WebDriverWait(browser, 40).until(
+            EC.presence_of_element_located((By.ID, 'notion-app'))
+        )
 
-try: 
-    time.sleep(5)
-    WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.TAG_NAME, 'body'))) 
-    print("Страница успешно загружена") 
+        # buttons = browser.find_elements(By.CSS_SELECTOR, 'div[role="button"]')
+        # for button in buttons:
+        #     try:
+        #         button.click()
+        #         print(f"Клик по кнопке: {button.text}")
+        #     except Exception as e:
+        #         print(f"Не удалось кликнуть по кнопке: {e}")
 
-    notion_app_element = WebDriverWait(browser, 40).until(
-        EC.presence_of_element_located((By.ID, 'notion-app'))
-    )
+        content = notion_app_element.text
 
-    # buttons = browser.find_elements(By.CSS_SELECTOR, 'div[role="button"]')
-    # for button in buttons:
-    #     try:
-    #         button.click()
-    #         print(f"Клик по кнопке: {button.text}")
-    #     except Exception as e:
-    #         print(f"Не удалось кликнуть по кнопке: {e}")
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(content)
 
-    print(notion_app_element.text)
-except Exception as e: 
-    print(f"Не удалось загрузить страницу или найти элемент {e}") 
-finally:
-    browser.close()
-    browser.quit()
+        print(f"Содержимое сохранено в файл: {file_path}")
+
+    except Exception as e:
+        print(f"Не удалось загрузить страницу или найти элемент {e}")
+    finally:
+        browser.close()
+        browser.quit()
+
+    return os.path.abspath(file_path)
+
+# Пример использования функции
+url = 'https://quickest-custard-3d3.notion.site/Dmitry-Konoplyannikov-7892b63ba7cb4000b45484147a783bf0'
+file_path = fetch_and_save_notion_content(url)
+print(f"Путь к файлу: {file_path}")
