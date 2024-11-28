a68ef9dbf207286e25337f5f7cac6c3d37e257f2
Kin1599 <dkonopliannikov05@mail.ru>
2024-11-28 14:49:18 +0300

    add tags for routers

--- backend/src/constructor/router.py
+++ backend/src/constructor/router.py
@@ -34,78 +34,78 @@ router = APIRouter(
     prefix='/config'
 )
 
-@router.get('/sources')
+@router.get('/sources', tags=['Source'])
 def get_list_sources(db: Session = Depends(get_session)):
     sources = CRUDSource.get_sources(db)
     return sources
 
-@router.post('/sources')
+@router.post('/sources', tags=['Source'])
 def add_source(source: SourceCreate, db: Session = Depends(get_session)):
     new_source = CRUDSource.create_source(db, source)
     return new_source
 
-@router.put('/sources/{id_source}')
+@router.put('/sources/{id_source}', tags=['Source'])
 def change_source(id_source: int, source_update: SourceUpdate, db: Session = Depends(get_session)):
     updated_source = CRUDSource.update_source(db, id_source, source_update)
     if not updated_source:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Source not found!")
     return updated_source
 
-@router.delete('/sources/{id_source}')
+@router.delete('/sources/{id_source}', tags=['Source'])
 def delete_source(id_source: int, db: Session = Depends(get_session)):
     deleted_source = CRUDSource.delete_source(db, id_source)
     if not deleted_source:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Source not found")
     return deleted_source
 
-@router.get('/assistants')
+@router.get('/assistants', tags=['Assistant'])
 def get_assistants(db: Session = Depends(get_session)):
     assistants = CRUDAssistant.get_assistants(db)
     return assistants
 
-@router.get('/assistants/{id_assistant}')
+@router.get('/assistants/{id_assistant}', tags=['Assistant'])
 def get_assistant_by_id(id_assistant: int, db: Session = Depends(get_session)):
     assistant = CRUDAssistant.get_assistant(db, id_assistant)
     if not assistant:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Assistant not found')
     return assistant
 
-@router.post('/assistants')
+@router.post('/assistants', tags=['Assistant'])
 def save_assistant(assistant: AssistantCreate, db: Session = Depends(get_session)):
     new_assistant = CRUDAssistant.create_assistant(db, assistant)
     return new_assistant
 
-@router.patch('/assistants/{id_assistant}')
+@router.patch('/assistants/{id_assistant}', tags=['Assistant'])
 def change_settings_assistant(id_assistant: int, assistant_update: AssistantUpdate, db: Session = Depends(get_session)):
     updated_assistant = CRUDAssistant.update_assistant(db, id_assistant, assistant_update)
     if not updated_assistant:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Assistant not found')
     return updated_assistant
 
-@router.delete('/assistants/{id_assistant}')
+@router.delete('/assistants/{id_assistant}', tags=['Assistant'])
 def delete_assistant(id_assistant: int, db: Session = Depends(get_session)):
     deleted_assistant = CRUDAssistant.delete_assistant(db, id_assistant)
     if not deleted_assistant:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Assistant not found')
     return delete_assistant
 
-@router.get('/assistants/{id_assistant}/export')
+@router.get('/assistants/{id_assistant}/export', tags=['Assistant'])
 def get_code_assistant(id_assistant: int):
     pass
 
-@router.get('/llm')
+@router.get('/llm', tags=['Llm'])
 def get_all_llm_models():
     pass
 
-@router.get('/llm/{id_llm}/settings')
+@router.get('/llm/{id_llm}/settings', tags=['Llm'])
 def get_settings_llm_by_id(id_llm: int):
     pass
 
-@router.patch('/llm/{id_llm}/settings')
+@router.patch('/llm/{id_llm}/settings', tags=['Llm'])
 def change_settings_llm_by_id(id_llm: int):
     pass
 
-@router.post('/chat')
+@router.post('/chat', tags=['RAG'])
 def chat(request: ChatRequest):
     try:
         answer, source_docs = bot.chat(request.question)
@@ -114,7 +114,7 @@ def chat(request: ChatRequest):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 
-@router.post("/addServicesAndPrompt")
+@router.post("/addServicesAndPrompt", tags=['RAG'])
 async def upload_file(request_data: RequestData):
     sources_to_load = []
     for service_type, urls in request_data.services.model_dump().items():
@@ -126,7 +126,7 @@ async def upload_file(request_data: RequestData):
 
     return {"results": f"services and prompt added"}
 
-@router.post("/uploadfiles/")
+@router.post("/uploadfiles/", tags=['RAG'])
 async def create_upload_files(files: list[UploadFile]):
     sources_to_load = []
     for file in files:
